1a6e64473ab3c7817319558f8e982ea61373885f
KashanMehmood <kashan.mehmood13@gmail.com>
2020-03-29 14:16:49 +0500

    React Native Initialized

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>
+        Hello React Native
+      </Text>
+    </View>
+  );
+};
+
+export default App;
